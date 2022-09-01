d360dfcced3d1600570c8de304b3ebfdbea67ea0
ShinyParadise <shinodise@gmail.com>
2022-09-01 16:35:14 +0300

    dto class simplified to data class

--- app/src/main/java/com/example/toDoListKotlin/dto/ToDoList.kt
+++ app/src/main/java/com/example/toDoListKotlin/dto/ToDoList.kt
@@ -1,16 +1,3 @@
 package com.example.toDoListKotlin.dto
 
-import java.util.*
-
-class ToDoList(var header: String, var description: String) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
-        val toDoList = other as ToDoList
-        return if (header != toDoList.header) false else description == toDoList.description
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(header, description)
-    }
-}
+data class ToDoList(var header: String, var description: String) {}
