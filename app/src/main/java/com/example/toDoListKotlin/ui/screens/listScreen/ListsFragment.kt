e4ade5e005d1c8485136fff31cfd49c83a9d1994
ShinyParadise <shinodise@gmail.com>
2022-09-05 11:59:31 +0300

    fixed fragment problem

--- app/src/main/java/com/example/toDoListKotlin/ui/screens/listScreen/ListsFragment.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/screens/listScreen/ListsFragment.kt
@@ -6,11 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import com.example.toDoListKotlin.dto.ToDoList
 import com.example.toDoListKotlin.ui.theme.ToDoListAppTheme
 
-// TODO: use fragment instead of activity
 class ListsFragment : Fragment() {
     private val listViewModel: ListViewModel = ListViewModel()
 
--- app/src/main/java/com/example/toDoListKotlin/ui/startup/MainActivity.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/startup/MainActivity.kt
@@ -1,26 +1,19 @@
 package com.example.toDoListKotlin.ui.startup
 
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.example.toDoListKotlin.ui.screens.listScreen.ListViewModel
-import com.example.toDoListKotlin.ui.screens.listScreen.ListsLiveDataComponent
-import com.example.toDoListKotlin.ui.theme.ToDoListAppTheme
+import com.example.toDoListKotlin.R
+import com.example.toDoListKotlin.ui.screens.listScreen.ListsFragment
 
-// Пока что первый экран - в активити, потому что с транзакциями фрагментов возникла
-// ошибочка (incorrect fragment container id - вроде все понятно, но айди неверным
-// быть не может, я его брал и из R класса и пробовал через вью биндинг, но ничего
-// не помогло)
 class MainActivity : AppCompatActivity() {
-    private val listViewModel: ListViewModel = ListViewModel()
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        setContent {
-            ToDoListAppTheme {
-                ListsLiveDataComponent(listsLiveData = listViewModel.toDoLists)
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, ListsFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
