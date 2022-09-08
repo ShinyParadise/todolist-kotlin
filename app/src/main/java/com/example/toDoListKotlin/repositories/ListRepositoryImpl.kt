080dad30986735ad3d20baedc0f91be4e99de284
ShinyParadise <shinodise@gmail.com>
2022-09-08 18:28:14 +0300

    repo implemented

---
+++ app/src/main/java/com/example/toDoListKotlin/repositories/ListRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.example.toDoListKotlin.repositories
+
+import com.example.toDoListKotlin.db.AppDatabase
+import com.example.toDoListKotlin.db.dao.ToDoListDAO
+import com.example.toDoListKotlin.dto.ToDoList
+import com.example.toDoListKotlin.db.entities.ToDoList as dbList
+
+class ListRepositoryImpl(private val appDatabase: AppDatabase): ListRepository {
+    override suspend fun loadAll(): List<ToDoList> {
+        val listEntities = appDatabase.toDoListDAO().getAll()
+
+        val toDoLists = listEntities.map {
+            ToDoList(it.id, it.name, it.description)
+        }
+
+        return toDoLists
+    }
+
+    override suspend fun add(list: ToDoList): ToDoList {
+        val dbList = dbList(list.listID, list.name, list.description)
+        val insertedRowID = appDatabase.toDoListDAO().insert(dbList)
+
+        return list.copy(listID = insertedRowID)
+    }
+}
