358b43d9d70709a709206b36597e9e47dede3f8a
ShinyParadise <shinodise@gmail.com>
2022-09-08 18:51:49 +0300

    small primary key fix

--- app/src/main/java/com/example/toDoListKotlin/db/entities/ToDoList.kt
+++ app/src/main/java/com/example/toDoListKotlin/db/entities/ToDoList.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "lists")
 data class ToDoList(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val name: String,
     val description: String?
 )
