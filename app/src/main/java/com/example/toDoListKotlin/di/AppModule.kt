ef2f2210d23e6fb93e18785600594a63103c4d58
ShinyParadise <shinodise@gmail.com>
2022-09-12 14:13:43 +0300

    added DI modules

---
+++ app/src/main/java/com/example/toDoListKotlin/di/AppModule.kt
@@ -0,0 +1,17 @@
+package com.example.toDoListKotlin.di
+
+import com.example.toDoListKotlin.repositories.ListRepository
+import com.example.toDoListKotlin.repositories.ListRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+    @Singleton
+    @Binds
+    abstract fun provideListsRepository(repository: ListRepositoryImpl): ListRepository
+}
