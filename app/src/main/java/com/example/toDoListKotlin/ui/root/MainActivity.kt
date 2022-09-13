6ecebe275424a0c8cf1e174b820b4b37dfbc6921
ShinyParadise <shinodise@gmail.com>
2022-09-13 14:01:48 +0300

    activity starts 2nd screen (testing)

--- app/src/main/java/com/example/toDoListKotlin/ui/root/MainActivity.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/root/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.toDoListKotlin.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.toDoListKotlin.R
+import com.example.toDoListKotlin.ui.screens.detailScreen.DetailFragment
 import com.example.toDoListKotlin.ui.screens.listScreen.ListsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container_view, ListsFragment())
+            .replace(R.id.fragment_container_view, DetailFragment())
             .commit()
     }
 }
