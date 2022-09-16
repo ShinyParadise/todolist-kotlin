604073f032132d87058a9247cbfddc2732b51000
ShinyParadise <shinodise@gmail.com>
2022-09-16 10:30:28 +0300

    refactored 1st screen

--- app/src/main/java/com/example/toDoListKotlin/ui/screens/listScreen/ListsFragment.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/screens/listScreen/ListsFragment.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.example.toDoListKotlin.ui.screens.alertDialog.CustomDialog
 import com.example.toDoListKotlin.ui.theme.ToDoListAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,21 +21,8 @@ class ListsFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                var openDialog by remember { mutableStateOf(false) }
-
                 ToDoListAppTheme {
-                    ListsScreen(listViewModel = viewModel) {
-                        openDialog = true
-                    }
-
-                    if (openDialog) {
-                        CustomDialog(
-                            inputFields = arrayOf("Name", "Description"),
-                            title = "Add a list",
-                            onPositiveClick = { openDialog = false },
-                            onNegativeClick = { openDialog = false }
-                        )
-                    }
+                    ListsScreen(listViewModel = viewModel)
                 }
             }
         }
