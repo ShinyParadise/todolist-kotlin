21f24172752ff538cf224ac7ba35e9705b881b57
ShinyParadise <shinodise@gmail.com>
2022-09-19 15:29:09 +0300

    change state test added

--- app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
+++ app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
@@ -1,17 +1,22 @@
 package com.example.toDoListKotlin
 
+import com.example.toDoListKotlin.ui.screens.detailScreen.DetailViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class DetailViewModelUnitTest {
+    private lateinit var sut: DetailViewModel
+
+    @Before
+    fun setup() {
+        sut = DetailViewModel()
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test change item state`() {
+        sut.changeSavedItemState()
+        assertTrue(sut.savedItemState.value)
     }
 }
