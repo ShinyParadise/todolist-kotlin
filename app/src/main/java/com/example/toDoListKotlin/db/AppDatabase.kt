19b06c5cec61aa62c685e13e5a87a73def7ff9d4
ShinyParadise <shinodise@gmail.com>
2022-09-19 15:18:37 +0300

    new entity, room db migration

--- app/src/main/java/com/example/toDoListKotlin/db/AppDatabase.kt
+++ app/src/main/java/com/example/toDoListKotlin/db/AppDatabase.kt
@@ -1,11 +1,19 @@
 package com.example.toDoListKotlin.db
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.toDoListKotlin.db.dao.ToDoListDAO
+import com.example.toDoListKotlin.db.entities.ListItem
 import com.example.toDoListKotlin.db.entities.ToDoList
 
-@Database(version = 1, entities = [ToDoList::class])
+@Database(
+    version = 2,
+    entities = [ToDoList::class, ListItem::class],
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun toDoListDAO(): ToDoListDAO
 }
