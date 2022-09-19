2d191dd432a082d1dc9b63948a2bba0d7e22541f
ShinyParadise <shinodise@gmail.com>
2022-09-19 12:49:25 +0300

    first screen on launch

--- app/src/main/java/com/example/toDoListKotlin/ui/root/MainActivity.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/root/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.toDoListKotlin.ui.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.toDoListKotlin.R
-import com.example.toDoListKotlin.ui.screens.detailScreen.DetailFragment
 import com.example.toDoListKotlin.ui.screens.listScreen.ListsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container_view, DetailFragment())
+            .replace(R.id.fragment_container_view, ListsFragment())
             .commit()
     }
 }
