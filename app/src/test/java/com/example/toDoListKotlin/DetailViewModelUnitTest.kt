9b24180594b83bc7989637a1222019578e89f88d
ShinyParadise <shinodise@gmail.com>
2022-09-20 14:35:56 +0300

    test fix

--- app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
+++ app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
@@ -1,17 +1,37 @@
 package com.example.toDoListKotlin
 
+import com.example.toDoListKotlin.dto.ToDoList
+import com.example.toDoListKotlin.repositories.ListItemRepository
 import com.example.toDoListKotlin.ui.screens.detailScreen.DetailViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.mockito.Mockito.mock
 
 class DetailViewModelUnitTest {
     private lateinit var sut: DetailViewModel
+    private val testToDoList = ToDoList(1, "test", "test")
 
+    private val dispatcher = TestCoroutineDispatcher()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setup() {
-        sut = DetailViewModel()
+        Dispatchers.setMain(dispatcher)
+        val mockRepository = mock(ListItemRepository::class.java)
+        sut = DetailViewModel(repository = mockRepository, toDoList = testToDoList)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
