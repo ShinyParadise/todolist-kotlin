5a32fd32f6755b6de72173c7b93dc07d5a7d27e3
ShinyParadise <shinodise@gmail.com>
2022-09-20 16:51:54 +0300

    repository updated

--- app/src/main/java/com/example/toDoListKotlin/repositories/ListItemRepository.kt
+++ app/src/main/java/com/example/toDoListKotlin/repositories/ListItemRepository.kt
@@ -1,9 +1,10 @@
 package com.example.toDoListKotlin.repositories
 
 import com.example.toDoListKotlin.dto.ListItem
+import com.example.toDoListKotlin.dto.ToDoList
 
 interface ListItemRepository {
-    suspend fun loadAll(): List<ListItem>
+    suspend fun loadAll(toDoList: ToDoList): List<ListItem>
     suspend fun add(listItem: ListItem): ListItem
     suspend fun update(listItem: ListItem)
 }
--- app/src/main/java/com/example/toDoListKotlin/repositories/ListItemRepositoryImpl.kt
+++ app/src/main/java/com/example/toDoListKotlin/repositories/ListItemRepositoryImpl.kt
@@ -2,13 +2,14 @@ package com.example.toDoListKotlin.repositories
 
 import com.example.toDoListKotlin.db.dao.ListItemDAO
 import com.example.toDoListKotlin.dto.ListItem
+import com.example.toDoListKotlin.dto.ToDoList
 import javax.inject.Inject
 import com.example.toDoListKotlin.db.entities.ListItem as dbListItem
 
 class ListItemRepositoryImpl @Inject constructor (private val dao: ListItemDAO)
     : ListItemRepository {
-    override suspend fun loadAll(): List<ListItem> {
-        val listItemEntities = dao.getAll()
+    override suspend fun loadAll(toDoList: ToDoList): List<ListItem> {
+        val listItemEntities = dao.getAllListItems(toDoList.id.toString())
 
         val listItems = listItemEntities.map {
             ListItem(it.id, it.description, it.isChecked, it.listID)
@@ -27,5 +28,14 @@ class ListItemRepositoryImpl @Inject constructor (private val dao: ListItemDAO)
         val insertedRowID = dao.insert(dbListItem)
 
         return listItem.copy(id = insertedRowID)
+    }
+
+    override suspend fun update(listItem: ListItem) {
+        dao.update(dbListItem(
+            id = listItem.id,
+            description = listItem.description,
+            isChecked = listItem.state,
+            listID = listItem.listID
+        ))
     }
 }
