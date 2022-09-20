8c46b46d8890a3fa269e4e8107565d5f69b9e73e
ShinyParadise <shinodise@gmail.com>
2022-09-20 15:44:11 +0300

    load all test fix

--- app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
+++ app/src/test/java/com/example/toDoListKotlin/DetailViewModelUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.toDoListKotlin
 
+import com.example.toDoListKotlin.dto.ListItem
 import com.example.toDoListKotlin.dto.ToDoList
 import com.example.toDoListKotlin.repositories.ListItemRepository
 import com.example.toDoListKotlin.ui.screens.detailScreen.DetailViewModel
@@ -7,6 +8,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -20,6 +22,7 @@ class DetailViewModelUnitTest {
     private lateinit var sut: DetailViewModel
     private lateinit var mockRepository: ListItemRepository
     private val testToDoList = ToDoList(1, "test", "test")
+    private val testListItem = ListItem(1, "description", false, testToDoList.id)
 
     private val dispatcher = TestCoroutineDispatcher()
 
@@ -45,8 +48,14 @@ class DetailViewModelUnitTest {
 
     @Test
     fun `test add one item`() {
-        sut.setSavedDescription("teest")
-        coEvery { mockRepository.loadAll() } returns emptyList()
+        coEvery { mockRepository.add(any()) } returns testListItem
+        coEvery { mockRepository.loadAll() } returns listOf(testListItem)
 
+        sut.setSavedDescription("teest")
+        runBlocking {
+            sut.addListItem()
+        }
+
+        assertEquals(listOf(testListItem), sut.listItems.value)
     }
 }
