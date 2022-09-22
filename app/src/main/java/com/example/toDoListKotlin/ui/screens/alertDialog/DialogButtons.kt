f7bb4d2929f9a86d1b8fbc5dc8e301a8248f6559
ShinyParadise <shinodise@gmail.com>
2022-09-22 17:14:45 +0300

    buttons preview

--- app/src/main/java/com/example/toDoListKotlin/ui/screens/alertDialog/DialogButtons.kt
+++ app/src/main/java/com/example/toDoListKotlin/ui/screens/alertDialog/DialogButtons.kt
@@ -1,10 +1,14 @@
 package com.example.toDoListKotlin.ui.screens.alertDialog
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.toDoListKotlin.ui.theme.ToDoListAppTheme
 
 @Composable
 fun BackButton(onClick: () -> Unit) {
@@ -23,5 +27,27 @@ fun AddButton(onClick: () -> Unit) {
         Modifier.fillMaxWidth(1f)
     ) {
         Text(text = "Add")
+    }
+}
+
+@Preview
+@Composable
+private fun Buttons_Preview_Light() {
+    ToDoListAppTheme {
+        Row {
+            BackButton {}
+            AddButton {}
+        }
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun Buttons_Preview_Dark() {
+    ToDoListAppTheme {
+        Row {
+            BackButton {}
+            AddButton {}
+        }
     }
 }
